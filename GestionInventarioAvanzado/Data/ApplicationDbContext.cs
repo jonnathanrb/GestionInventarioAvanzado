ae8be0358e06b3330e7cff93234ec28ddeeaf228
jrodriguez@sofnomic.com <jrodriguez@sofnomic.com>
2024-11-10 19:48:31 -0600

    Creado el modelo Provider

---
+++ GestionInventarioAvanzado/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GestionInventarioAvanzado.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
---
+++ GestionInventarioAvanzado/Models/Provider.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionInventarioAvanzado.Models
+{
+    public class Provider
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Contact { get; set; }
+        public string Direction { get; set; }
+
+    }
+}
